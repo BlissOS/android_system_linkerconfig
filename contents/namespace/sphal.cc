1e54bedfe86456a36de8a1a2fba91850debd811f
hmtheboy154 <buingoc67@gmail.com>
2024-11-11 13:07:12 -0500

    Revert "add namespace change for houdini"
    This reverts commit 99fe7aa09840363cc0fd5c3bb0ff7ab35636baab.

--- contents/namespace/sphal.cc
+++ contents/namespace/sphal.cc
@@ -53,9 +53,6 @@ Namespace BuildSphalNamespace([[maybe_unused]] const Context& ctx) {
   ns.AddPermittedPath("/vendor/odm/${LIB}");
   ns.AddPermittedPath("/system/vendor/${LIB}");
 
-  ns.AddSearchPath("/vendor/${LIB}/arm");
-  ns.AddPermittedPath("/vendor/${LIB}/arm");
-  ns.AddSearchPath("/vendor/${LIB}/arm/nb");
   // TODO(b/326839235) Remove access to data once renderscript is deprecated.
   if (!android::linkerconfig::modules::IsVendorVndkVersionDefined()) {
     ns.AddPermittedPath("/data");
